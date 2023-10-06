c1f174c83468af3f9f543817da5982d3f8d0bd25
sachingimhan <sachingimhan8@gmail.com>
2023-10-06 22:09:49 +0530

    add store APIs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 const { checkAuth } = require('./src/middleware/auth');
 
@@ -17,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
